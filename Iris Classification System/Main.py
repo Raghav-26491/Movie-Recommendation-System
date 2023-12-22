51f03b3e941db314dcb061ab6bf6c48198b4f9f5
Raghav Puri <145380406+Raghav-26491@users.noreply.github.com>
2023-12-23 01:21:22 +0530

    Update Main.py

--- Iris Classification System/Main.py
+++ Iris Classification System/Main.py
@@ -29,82 +29,82 @@ print(df.isnull().sum())
 
 # Data Analysis
 # Plot histograms
-# plt.figure(figsize=(8, 6))
-#
-# plt.subplot(221)
-# df['SepalLengthCm'].hist()
-# plt.xlabel('Sepal Length (cm)')
-# plt.ylabel('Frequency')
-# plt.title('Histogram of Sepal Length')
-#
-# plt.subplot(222)
-# df['SepalWidthCm'].hist()
-# plt.xlabel('Sepal Width (cm)')
-# plt.ylabel('Frequency')
-# plt.title('Histogram of Sepal Width')
-#
-# plt.subplot(223)
-# df['PetalLengthCm'].hist()
-# plt.xlabel('Petal Length (cm)')
-# plt.ylabel('Frequency')
-# plt.title('Histogram of Petal Length')
-#
-# plt.subplot(224)
-# df['PetalWidthCm'].hist()
-# plt.xlabel('Petal Width (cm)')
-# plt.ylabel('Frequency')
-# plt.title('Histogram of Petal Width')
-#
-# plt.tight_layout()
-# plt.figure()
-#
-# # Create list of colors and class labels
-# colors = ['red', 'orange', 'blue']
-# species = ['Iris-virginica', 'Iris-versicolor', 'Iris-setosa']
-#
-# for i in range(3):
-#     # filter data on each class
-#     x = df[df['Species'] == species[i]]
-#     # plot the scatter plot
-#     plt.scatter(x['SepalLengthCm'], x['SepalWidthCm'], c=colors[i], label=species[i])
-#
-# plt.xlabel("Sepal Length")
-# plt.ylabel("Sepal Width")
-# plt.legend()
-# plt.figure()
-#
-# for i in range(3):
-#     # filter data on each class
-#     x = df[df['Species'] == species[i]]
-#     # plot the scatter plot
-#     plt.scatter(x['PetalLengthCm'], x['PetalWidthCm'], c=colors[i], label=species[i])
-#
-# plt.xlabel("Petal Length")
-# plt.ylabel("Petal Width")
-# plt.legend()
-# plt.figure()
-#
-# for i in range(3):
-#     # filter data on each class
-#     x = df[df['Species'] == species[i]]
-#     # plot the scatter plot
-#     plt.scatter(x['PetalLengthCm'], x['SepalLengthCm'], c=colors[i], label=species[i])
-#
-# plt.xlabel("Petal Length")
-# plt.ylabel("Sepal Length")
-# plt.legend()
-# plt.figure()
-#
-# for i in range(3):
-#     # filter data on each class
-#     x = df[df['Species'] == species[i]]
-#     # plot the scatter plot
-#     plt.scatter(x['SepalWidthCm'], x['PetalWidthCm'], c=colors[i], label=species[i])
-#
-# plt.xlabel("Sepal Width")
-# plt.ylabel("Petal Width")
-# plt.legend()
-# plt.show()
+plt.figure(figsize=(8, 6))
+
+plt.subplot(221)
+df['SepalLengthCm'].hist()
+plt.xlabel('Sepal Length (cm)')
+plt.ylabel('Frequency')
+plt.title('Histogram of Sepal Length')
+
+plt.subplot(222)
+df['SepalWidthCm'].hist()
+plt.xlabel('Sepal Width (cm)')
+plt.ylabel('Frequency')
+plt.title('Histogram of Sepal Width')
+
+plt.subplot(223)
+df['PetalLengthCm'].hist()
+plt.xlabel('Petal Length (cm)')
+plt.ylabel('Frequency')
+plt.title('Histogram of Petal Length')
+
+plt.subplot(224)
+df['PetalWidthCm'].hist()
+plt.xlabel('Petal Width (cm)')
+plt.ylabel('Frequency')
+plt.title('Histogram of Petal Width')
+
+plt.tight_layout()
+plt.figure()
+
+# Create list of colors and class labels
+colors = ['red', 'orange', 'blue']
+species = ['Iris-virginica', 'Iris-versicolor', 'Iris-setosa']
+
+for i in range(3):
+    # filter data on each class
+    x = df[df['Species'] == species[i]]
+    # plot the scatter plot
+    plt.scatter(x['SepalLengthCm'], x['SepalWidthCm'], c=colors[i], label=species[i])
+
+plt.xlabel("Sepal Length")
+plt.ylabel("Sepal Width")
+plt.legend()
+plt.figure()
+
+for i in range(3):
+    # filter data on each class
+    x = df[df['Species'] == species[i]]
+    # plot the scatter plot
+    plt.scatter(x['PetalLengthCm'], x['PetalWidthCm'], c=colors[i], label=species[i])
+
+plt.xlabel("Petal Length")
+plt.ylabel("Petal Width")
+plt.legend()
+plt.figure()
+
+for i in range(3):
+    # filter data on each class
+    x = df[df['Species'] == species[i]]
+    # plot the scatter plot
+    plt.scatter(x['PetalLengthCm'], x['SepalLengthCm'], c=colors[i], label=species[i])
+
+plt.xlabel("Petal Length")
+plt.ylabel("Sepal Length")
+plt.legend()
+plt.figure()
+
+for i in range(3):
+    # filter data on each class
+    x = df[df['Species'] == species[i]]
+    # plot the scatter plot
+    plt.scatter(x['SepalWidthCm'], x['PetalWidthCm'], c=colors[i], label=species[i])
+
+plt.xlabel("Sepal Width")
+plt.ylabel("Petal Width")
+plt.legend()
+plt.figure()
 
 # Display the Correlation Matrix
 from sklearn.preprocessing import LabelEncoder
@@ -121,9 +121,9 @@ print(correlation_matrix)
 
 # corr = df.corr()
 # # plot the heat map
-# fig, ax= plt.subplots(figsize=(5,4))
-# sns.heatmap(corr, annot=True, ax=ax, cmap="coolwarm")
-# plt.show()
+fig, ax= plt.subplots(figsize=(5,4))
+sns.heatmap(corr, annot=True, ax=ax, cmap="coolwarm")
+plt.show()
 
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
